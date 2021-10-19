52345006a7ae1b2e49cf6dac33e3c114a800029f
Ezequiel Leanes <ezequiel.leanes@gmail.com>
2021-10-19 20:19:24 -0300

    firebase integration

--- src/components/NewsForm/index.js
+++ src/components/NewsForm/index.js
@@ -1,28 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import {
-  Container, Typography, TextField, Button,
+  Container, Typography, TextField, Button, Alert, Snackbar, CircularProgress,
 } from '@material-ui/core';
+import { addDoc, collection, Timestamp } from 'firebase/firestore';
 import { useStyles } from './styles';
+import { db } from '../../firebase';
+import { useValidate } from '../../hooks/useValidateInput';
 
 export const NewsForm = () => {
   const classes = useStyles();
   const [formValues, setFormValue] = useState({ title: '', body: '' });
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+  const [showErrorAlert, setShowErrorAlert] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const validateInput = () => {
-    if (!formValues.title || !formValues.body) {
-      console.log('valores requeridos');
-    }
-  };
-
-  useEffect(() => validateInput(), [formValues]);
+  const isValid = useValidate(formValues);
 
   const onValueChange = (e) => {
     const { target: { value, name } } = e;
     setFormValue({ ...formValues, [name]: value });
   };
 
-  const onSendData = () => {
-    console.log('sent');
+  const onSendData = async () => {
+    setIsLoading(true);
+    try {
+      await addDoc(collection(db, 'news'), { ...formValues, date: Timestamp.fromDate(new Date()) });
+      setShowSuccessAlert(true);
+      setIsLoading(false);
+    } catch (e) {
+      setShowErrorAlert(true);
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -30,7 +38,32 @@ export const NewsForm = () => {
       <Typography variant="h6">Agregar Noticia</Typography>
       <TextField label="Titulo" size="small" value={formValues.title} onChange={onValueChange} name="title" required margin="normal" />
       <TextField label="Contenido" size="small" value={formValues.body} onChange={onValueChange} name="body" required margin="normal" multiline minRows={4} />
-      <Button variant="contained" onClick={onSendData}>Enviar</Button>
+      {isLoading ? (
+        <CircularProgress
+          size={30}
+          sx={{
+            alignSelf: 'center',
+          }}
+        />
+      ) : <Button variant="contained" onClick={onSendData} disabled={!isValid}>Enviar</Button>}
+      <Snackbar
+        open={showSuccessAlert}
+        autoHideDuration={3000}
+        onClose={() => setShowSuccessAlert(false)}
+      >
+        <Alert onClose={() => setShowSuccessAlert(false)} severity="success" sx={{ width: '100%' }}>
+          La noticia se subió con éxito
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={showErrorAlert}
+        autoHideDuration={3000}
+        onClose={() => setShowErrorAlert(false)}
+      >
+        <Alert onClose={() => setShowErrorAlert(false)} severity="error" sx={{ width: '100%' }}>
+          Hubo un error
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };
