8c0b1e9a5504943363e3879e090a36b9d1fc7236
roperrott <rosarioperrotta7@gmail.com>
2021-10-19 18:51:34 -0300

    validation added

--- src/components/NewsForm/index.js
+++ src/components/NewsForm/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Container, Typography, TextField, Button,
 } from '@material-ui/core';
@@ -8,16 +8,29 @@ export const NewsForm = () => {
   const classes = useStyles();
   const [formValues, setFormValue] = useState({ title: '', body: '' });
 
-  const onFormValueChange = (e) => {
+  const validateInput = () => {
+    if (!formValues.title || !formValues.body) {
+      console.log('valores requeridos');
+    }
+  };
+
+  useEffect(() => validateInput(), [formValues]);
+
+  const onValueChange = (e) => {
     const { target: { value, name } } = e;
     setFormValue({ ...formValues, [name]: value });
   };
+
+  const onSendData = () => {
+    console.log('sent');
+  };
+
   return (
     <Container className={classes.container}>
       <Typography variant="h6">Agregar Noticia</Typography>
-      <TextField label="Titulo" size="small" value={formValues.title} onChange={onFormValueChange} name="title" required margin="normal" />
-      <TextField label="Contenido" size="small" value={formValues.body} onChange={onFormValueChange} name="body" required margin="normal" multiline minRows={4} />
-      <Button variant="contained">Enviar</Button>
+      <TextField label="Titulo" size="small" value={formValues.title} onChange={onValueChange} name="title" required margin="normal" />
+      <TextField label="Contenido" size="small" value={formValues.body} onChange={onValueChange} name="body" required margin="normal" multiline minRows={4} />
+      <Button variant="contained" onClick={onSendData}>Enviar</Button>
     </Container>
   );
 };
